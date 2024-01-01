249032c3f8b365bf60fc58321e2739b2853eee43
deva40211 <devashish15262@gmail.com>
2024-01-01 18:00:44 +0530

    add pre production changes

--- api/index.js
+++ api/index.js
@@ -4,9 +4,20 @@ import dotenv from 'dotenv';
 import userRoutes from './route/user.route.js';
 import authRoutes from './route/auth.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
+const __dirname = path.resolve();
+// mechanism to locate the directory
+
 const app = express();
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+// find our build/dist folder (anywhere it may be)
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+// once in client, client/dist/index.html is sent to the client
 
 app.use(express.json());
 app.use(cookieParser());
