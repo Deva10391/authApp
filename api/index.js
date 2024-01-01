0bf507aa7e78d51d0549576d5b901dc80b799517
deva40211 <devashish15262@gmail.com>
2024-01-01 13:21:46 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
+
+mongoose
+    .connect(process.env.MONGO_URI || 'mongodb+srv://devashish15262:authP123@auth.jfurpdt.mongodb.net/authP123?retryWrites=true&w=majority')
+    .then(() => {
+        console.log('Connected')
+    })
+    .catch((err) => {
+        console.error(err);
+    });
 
 const PORT = 3000;
 
