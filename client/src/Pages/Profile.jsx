966a68988146509f07378d66e35ba62319b6ec94
deva40211 <devashish15262@gmail.com>
2024-01-02 05:38:24 +0530

    final commit

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -97,7 +97,7 @@ export default function Profile() {
   const handleSignOut = async () => {
     try {
       dispatch(signOutUserStart());
-      const res = await fetch('http://localhost:3000/api/auth/signout');
+      const res = await fetch(`/api/auth/signout`);
       const data = await res.json();
       if (data.success === false) {
         dispatch(signOutUserFailure(data.message));
